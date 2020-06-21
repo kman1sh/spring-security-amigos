6c94843be4ec6fa5072c4177d17aa31975c00e5e
manish <manish99150@gmail.com>
2020-06-21 13:45:14 +0530

    01 Spring security dependency implementation

---
+++ src/main/java/com/manish/springsecurityamigos/SpringsecurityAmigosApplication.java
@@ -0,0 +1,13 @@
+package com.manish.springsecurityamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityAmigosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityAmigosApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/manish/springsecurityamigos/student/StudentController.java
@@ -0,0 +1,29 @@
+package com.manish.springsecurityamigos.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1,"Manish Kumar"),
+            new Student(2,"Rohit Kaushal"),
+            new Student(3,"Sakshi Uniyal")
+    );
+
+    @GetMapping("{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " +studentId + " does not exists."));
+
+    }
+}
